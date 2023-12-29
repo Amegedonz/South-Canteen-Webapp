ebe9bc7a12e822c0dfa3c07dcef35af39c1fdaad
Lucian <lucian.m.ho@gmail.com>
2023-12-29 19:07:41 +0800

    test

--- Backend/Customer/CustomerClasses/customer_login.py
+++ Backend/Customer/CustomerClasses/customer_login.py
@@ -4,4 +4,5 @@ class CustomerLogin(C):
     def __init__(self, phoneNumber, password, rememberMe):
         super().__init__(phoneNumber, password)
         self.__rememberMe = rememberMe
-        #check flask-login manager
+        
+ #Impliment flask-login manager
